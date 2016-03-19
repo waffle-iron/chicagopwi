d0e565ebd326d39e78066da6f0e8e13bdec31bca
Jake Siddall <jacobsiddall@gmail.com>
2016-03-19 00:55:36 -0500

    adds about.html content

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,19 +13,21 @@ var App = function () {
 	// Fixed Header
 	function handleHeader() {
 		jQuery(window).scroll(function() {
-		  if (jQuery(window).scrollTop() > 100) {
-			jQuery('.header-fixed .header-sticky').addClass('header-fixed-shrink');
-			jQuery('.big-logo').addClass('hidden');
-			jQuery('.small-logo').removeClass('visible-xs-block').removeClass('visible-sm-block');
-			jQuery('.nav').removeClass('big-nav');
-			jQuery('#layerslider').removeClass('push-down');
-		  } else {
-			jQuery('.header-fixed .header-sticky').removeClass('header-fixed-shrink');
-			jQuery('.big-logo').removeClass('hidden');
-			jQuery('.small-logo').addClass('visible-xs-block').addClass('visible-sm-block');
-			jQuery('.nav').addClass('big-nav');
-			jQuery('#layerslider').addClass('push-down');
-		  }
+			if ($("body.home").length > 0){
+			  if (jQuery(window).scrollTop() > 100) {
+				jQuery('.header-fixed .header-sticky').addClass('header-fixed-shrink');
+				jQuery('.big-logo').addClass('hidden');
+				jQuery('.small-logo').removeClass('visible-xs-block').removeClass('visible-sm-block');
+				jQuery('.nav').removeClass('big-nav');
+				jQuery('#layerslider').removeClass('push-down');
+			  } else {
+				jQuery('.header-fixed .header-sticky').removeClass('header-fixed-shrink');
+				jQuery('.big-logo').removeClass('hidden');
+				jQuery('.small-logo').addClass('visible-xs-block').addClass('visible-sm-block');
+				jQuery('.nav').addClass('big-nav');
+				jQuery('#layerslider').addClass('push-down');
+			  }
+			}
 		});
 	}
 
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,24 @@
-/* Write here your custom javascript codes */
+$( document ).ready(function() {
+  if ($("body.home").length > 0){
+  	$('.nav_home').addClass('active');
+  	$('.big-logo').removeClass('hidden');
+  	$('.header-sticky').removeClass('header-fixed-shrink');
+  	$('.small-logo').addClass('visible-xs-block').addClass('visible-sm-block');
+  	$('.nav.navbar-nav').addClass('big-nav');
+  }
+  else if ($("body.services").length > 0){
+  	$('.nav_services').addClass('active');
+  }
+  else if ($("body.about").length > 0){
+  	$('.nav_about').addClass('active');
+  }
+  else if ($("body.billing").length > 0){
+  	$('.nav_billing').addClass('active');
+  }
+  else if ($("body.faq").length > 0){
+  	$('.nav_faq').addClass('active');
+  }
+  else if ($("body.blog").length > 0){
+  	$('.nav_blog').addClass('active');
+  }
+});
